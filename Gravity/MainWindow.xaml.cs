c962bdad839abfa10e337f704bf6ea208e93364a
Øystein Kolsrud <yko@qlik.com>
2021-03-04 09:23:35 +0100

    Moved class Position to separate file.

--- Gravity/MainWindow.xaml.cs
+++ Gravity/MainWindow.xaml.cs
@@ -273,37 +273,4 @@ namespace Gravity
             UiElement.SetValue(Canvas.TopProperty, center.Y + (Position.Y - Radius) / MainWindow.Scale);
         }
     }
-
-    class Position
-    {
-        public double X;
-        public double Y;
-
-        public Position(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public Position((double x, double y) p) : this(p.x, p.y) { }
-
-        public double Distance(Position p)
-        {
-            return Distance((p.X, p.Y));
-        }
-        public double Distance((double x, double y) p)
-        {
-            var x = Math.Abs(X - p.x);
-            var y = Math.Abs(Y - p.y);
-
-            return Math.Sqrt(x * x + y * y);
-        }
-
-        public double Angle(Position p)
-        {
-            var x = p.X - X;
-            var y = p.Y - Y;
-            return Math.Atan2(y, x);
-        }
-    }
 }
